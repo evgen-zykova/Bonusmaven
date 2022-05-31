8b77b2f35d858cfd85b3e1fa5b59f66671ba7feb
Евгения Пискарева <evgen-zykova@mail.ru>
2022-06-01 01:14:44 +0300

    Initial commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,12 +2,12 @@ public class main {
     public static void main(String[] args) {
         BonusService service = new BonusService();
 
-        int expected = 150;
-        int actual = (int) service.calculate(5_000, true);
+        long expected = 150;
+        long actual = (long) service.calculate(5_000, true);
         System.out.println("1. " + expected + " == ? == " + actual);
 
         expected = 50;
-        actual = (int) service.calculate(5_000, false);
+        actual = (long) service.calculate(5_000, false);
         System.out.println("2. " + expected + " == ? == " + actual);
 
     }
