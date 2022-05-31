acee0dd2ff629dc42859300a3eead8886aa2a1a1
Евгения Пискарева <evgen-zykova@mail.ru>
2022-06-01 00:48:04 +0300

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        int expected = 150;
+        int actual = (int) service.calculate(5_000, true);
+        System.out.println("1. " + expected + " == ? == " + actual);
+
+        expected = 50;
+        actual = (int) service.calculate(5_000, false);
+        System.out.println("2. " + expected + " == ? == " + actual);
+
+    }
+}
